059c2f3ab48bd9ce7ac9943bbf6d94a5cafd4d84
harsij01 <harsinij6530@gmail.com>
2026-02-09 20:21:26 +0530

    completed 6th exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let final_string = "";
 
+    if (num >= 0) {
+        for (i = 0; i < num; i++) {
+            final_string += string;
+        }
+    } else {
+        return "ERROR";
+    }
+
+    return final_string;
 };
 
 // Do not edit below this line
