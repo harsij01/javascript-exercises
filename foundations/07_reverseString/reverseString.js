fd8bf1b5a6641950bf7f06dc903f6973421033a2
harsij01 <harsinij6530@gmail.com>
2026-02-09 20:29:44 +0530

    completed 7th exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    finalString = "";
+    for (i = string.length - 1; i >= 0; i--) {
+        finalString += string[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
